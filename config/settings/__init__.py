b9c3b1bea4f7d313f679a5c784e57f7621b88fe8
Snake-Soft <info@snake-soft.com>
2020-04-18 15:23:04 +0200

    changed attributes default

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -3,5 +3,24 @@ Cut out important data from git repository
 base.py holds all default data
 data.py holds all private data (needs to be copied from data_sample.py first
 """
+from os import environ
 from .base import *
-from .data import ALLOWED_HOSTS, DEBUG, SECRET_KEY
+try:
+    import config.settings.data as DATA
+except ModuleNotFoundError:
+    DATA = False
+
+
+DEFAULT_DATA = {
+    'ALLOWED_HOSTS': [],
+    'DEBUG': False,
+    'SECRET_KEY': ''
+    }
+
+for attr, default in DEFAULT_DATA.items():
+    if DATA and hasattr(DATA, attr):
+        globals()[attr] = getattr(DATA, attr)
+    elif environ.get(attr):
+        globals()[attr] = environ.get(attr).split(',')
+    else:
+        globals()[attr] = DEFAULT_DATA.get(attr)
