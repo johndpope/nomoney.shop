57a68572c83b1198931d202c42476a01dd7df03c
Snake-Soft <info@snake-soft.com>
2020-04-18 20:18:53 +0200

    registration works

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm as _UserCreationForm,\
     AuthenticationForm
+from django.contrib.auth import get_user_model
+from user.models import User
 
 
 class UserCreationForm(_UserCreationForm):
-    pass
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
--- user/views.py
+++ user/views.py
@@ -4,8 +4,10 @@ from django.views.generic.detail import DetailView
 from listing.models import Listing
 from django.views.generic.edit import UpdateView, CreateView, FormView
 from django.urls.base import reverse_lazy
+from django.contrib.auth import login, authenticate
 from .models import User
 from .forms import UserCreationForm
+from django.shortcuts import redirect
 
 
 class CalculatorView(TemplateView):
@@ -40,3 +42,8 @@ class UserCreateView(FormView):
     form_class = UserCreationForm
     template_name = 'user/user_create.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
