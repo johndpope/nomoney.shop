ab8793309097181387f0a30f8cce6d20f0dd8f44
Snake-Soft <info@snake-soft.com>
2020-06-26 00:54:40 +0200

    error because of old dealset class

--- listing/views.py
+++ listing/views.py
@@ -78,9 +78,11 @@ class ListingDetailView(DetailView):  # OK
         listing = self.model.objects.get(pk=kwargs.get('pk'))
         user = request.user
         partner = listing.user
-        self.extra_context = {
-            'deal': user.get_dealset_from_partner(partner).deal
-            }
+        #=======================================================================
+        # self.extra_context = {
+        #     'deal': user.get_dealset_from_partner(partner).deal
+        #     }
+        #=======================================================================
         return DetailView.dispatch(self, request, *args, **kwargs)
 
 
