9f9704efb5e49b7554cc0be0d87b766abe457b03
Snake-Soft <info@snake-soft.com>
2020-06-28 17:50:42 +0200

    Better config import management

--- config/settings/parser.py
+++ config/settings/parser.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, environ
 from configparser import ConfigParser
 from django.core.management.utils import get_random_secret_key
 from . import BASE_DIR
@@ -6,7 +6,7 @@ from . import BASE_DIR
 CONFIG_PATH = path.join(BASE_DIR, '.config.txt')
 
 #[BASE]
-SECRET_KEY = ''
+SECRET_KEY = get_random_secret_key()
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -20,31 +20,42 @@ DB_CONFIG = {
     'PORT': '',
     }
 
+if 'SECRET_KEY' in environ.keys():
+    SECRET_KEY = str(environ.get('SECRET_KEY'))
+
+if 'DEBUG' in environ.keys():
+    DEBUG = eval(environ.get('DEBUG'))
+
+if 'ALLOWED_HOSTS' in environ.keys():
+    ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(',')
+
 
 if path.isfile(CONFIG_PATH):
     _parser = ConfigParser()
     _parser.read(CONFIG_PATH)
     if _parser.has_section('BASE'):
-        SECRET_KEY = str(_parser['BASE'].get(SECRET_KEY) or
+        _base = _parser['BASE']
+        SECRET_KEY = str(_base.get(SECRET_KEY) or
                          get_random_secret_key())
-        if 'DEBUG' in _parser['BASE']:
-            DEBUG = eval(_parser['BASE']['DEBUG'])
-        if 'ALLOWED_HOSTS' in _parser['BASE']:
-            ALLOWED_HOSTS = eval(_parser['BASE']['ALLOWED_HOSTS']) or ALLOWED_HOSTS
+        if 'DEBUG' in _base:
+            DEBUG = eval(_base['DEBUG'])
+        if 'ALLOWED_HOSTS' in _base:
+            ALLOWED_HOSTS = eval(_base['ALLOWED_HOSTS']) or ALLOWED_HOSTS
 
     if _parser.has_section('DATABASE'):
-        if 'TYPE' in _parser['DATABASE']:
-            DB_CONFIG['TYPE'] = str(_parser['DATABASE']['TYPE']) or DB_CONFIG['TYPE']
+        _database = _parser['DATABASE']
+        if 'TYPE' in _database:
+            DB_CONFIG['TYPE'] = str(_database['TYPE']) or DB_CONFIG['TYPE']
         if 'NAME' in _parser['DATABASE']:
-            DB_CONFIG['NAME'] = str(_parser['DATABASE']['NAME']) or DB_CONFIG['NAME']
+            DB_CONFIG['NAME'] = str(_database['NAME']) or DB_CONFIG['NAME']
         if 'USER' in _parser['DATABASE']:
-            DB_CONFIG['USER'] = str(_parser['DATABASE']['USER']) or DB_CONFIG['USER']
+            DB_CONFIG['USER'] = str(_database['USER']) or DB_CONFIG['USER']
         if 'PASSWORD' in _parser['DATABASE']:
-            DB_CONFIG['PASSWORD'] = str(_parser['DATABASE']['PASSWORD']) or DB_CONFIG['PASSWORD']
+            DB_CONFIG['PASSWORD'] = str(_database['PASSWORD']) or DB_CONFIG['PASSWORD']
         if 'HOST' in _parser['DATABASE']:
-            DB_CONFIG['HOST'] = str(_parser['DATABASE']['HOST']) or DB_CONFIG['HOST']
+            DB_CONFIG['HOST'] = str(_database['HOST']) or DB_CONFIG['HOST']
         if 'PORT' in _parser['DATABASE']:
-            DB_CONFIG['PORT'] = str(_parser['DATABASE']['PORT']) or DB_CONFIG['PORT']
+            DB_CONFIG['PORT'] = str(_database['PORT']) or DB_CONFIG['PORT']
     else:
         DB_CONFIG = None
 
