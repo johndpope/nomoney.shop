b5500acb796bdcd1c629334061523c5609ca463b
Snake-Soft <info@snake-soft.com>
2020-07-01 17:34:34 +0200

    global context and menu

--- config/global_context.py
+++ config/global_context.py
@@ -1,10 +1,28 @@
 from config.settings import VERSION, NAME
 
 
+SYMBOLS = {
+        'category': '<i class="fas fa-box"></i>',
+        'deal': '<i class="fas fa-balance-scale"></i>',
+        'user': '<i class="fas fa-user"></i>',
+        'guild': '<i class="fas fa-users"></i>',
+        'feedback': '<i class="fas fa-clipboard"></i>',
+        'feedback_regular': '<i class="far fa-clipboard"></i>',
+        'virtual': '<i class="fas fa-brain"></i>',
+        'listing': '<i class="fas fa-file-alt"></i>',
+        'bookmark': '<i class="fas fa-bookmark"></i>',
+        'search': '<i class="fas fa-search"></i>',
+        'chat': '<i class="fas fa-comments"></i>',
+        'chat_regular': '<i class="far fa-comments"></i>',
+        
+    }
+
+
 def default(request):
     """registered context processor"""
     ret = {}
     ret['version'] = VERSION
     ret['version_str'] = '.'.join((str(elem) for elem in VERSION))
     ret['name'] = NAME
+    ret['symbols'] = SYMBOLS
     return ret
--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $("#help_button").click(function(){
     	$("#help_block").toggle();
-    }); 
+    });
+    $("search_toggle").click(function(){
+    	$("#search_box").toggle();
+    });
 });
