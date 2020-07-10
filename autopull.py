bab273530f3a65db5dacb326fd89b50749b72b8c
Snake-Soft <info@snake-soft.com>
2020-07-10 21:46:00 +0200

    auto pip install -r

--- autopull.py
+++ autopull.py
@@ -15,6 +15,7 @@ def execute(cmd, directory):
 directory = '/var/www/nomoney.shop'
 result = execute('su www-data -c "git pull"', directory)
 if result[0] != u'Bereits aktuell.':
+    result = execute('su www-data -c "venv/bin/python pip install -r requirements.txt"', directory)
     result = execute('su www-data -c "venv/bin/python manage.py collectstatic --noinput"', directory)
     result = execute('su www-data -c "venv/bin/python manage.py compilemessages"', directory)
     result = execute('su www-data -c "venv/bin/python manage.py migrate"', directory)
