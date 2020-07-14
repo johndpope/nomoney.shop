1dffa7a4b26dd0246617857548607c84bd9073cc
Snake-Soft <info@snake-soft.com>
2020-07-14 21:21:49 +0200

    category status and admin panel

--- category/models.py
+++ category/models.py
@@ -2,11 +2,23 @@ from itertools import chain
 from django.db import models
 
 
+class CategoryStatus(models.IntegerChoices):
+    UNPROVED = 0, 'unproved'
+    PROVED = 10, 'proved'
+    HIDDEN = 20, 'hidden'
+    DELETED = 110, 'deleted'
+
+
 class Category(models.Model):
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.CASCADE
+        )
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
-    hidden = models.BooleanField(default=False)
+    status = models.PositiveSmallIntegerField(
+        default=CategoryStatus.UNPROVED,
+        choices=CategoryStatus.choices,
+        )
 
     @property
     def pushs(self):
@@ -36,6 +48,10 @@ class Category(models.Model):
             title = obj.parent.title + '/' + title
             obj = obj.parent
         return title
+
+    @classmethod
+    def get_unproved(cls):
+        return cls.objects.filter(status=CategoryStatus.UNPROVED)
 
     def __lt__(self, other):
         return len(self.listings) < len(other.listings)
